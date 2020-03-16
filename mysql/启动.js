97dcfab924ce3325821e8ac9e346f5945664da5d
JimmyLLLL <851664281@qq.com>
2020-03-16 21:40:47 +0800

    update

---
+++ mysql/启动.js
@@ -0,0 +1,3 @@
+const start = 'sudo /usr/local/mysql/support-files/mysql.server start'
+const oxX10116OldVersion = 'mysql5.7.16'
+const oldVersionNet = 'https://downloads.mysql.com/archives/community/'
---
+++ nodeJS/项目坑.js
@@ -0,0 +1,5 @@
+const taskControl = `
+npm install -g forever
+forever start app.js   //启动
+forever stop app.js   //关闭
+`
